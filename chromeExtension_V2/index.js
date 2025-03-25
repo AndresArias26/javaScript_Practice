f2b44c426ac0ed7cbc055c65f2b1c0925a7f2e00
Andres Arias <75452639+AndresArias26@users.noreply.github.com>
2025-03-24 21:25:07 -0400

    Add files via upload

---
+++ chromeExtension_V2/index.js
@@ -0,0 +1,33 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+const inputBtn = document.getElementById("input-btn")
+
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    // 5. Call the renderLeads() function
+    renderLeades()
+})
+
+// 4. Wrap the code below in a renderLeades() function
+
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin
+function renderLeades(){
+    let listItems = ""
+    for(let x = 0; x < myLeads.length; x++){
+        // 2. Add the item to the listItems variable instead of the ulEl. innerHTML
+        listItems += "<li>" + myLeads[x] + "</li>"
+        console.log(listItems)
+    }
+    
+    // 3.Render the listItems inside the unordered list using ulEt. innerHML
+    ulEl.innerHTML = listItems
+    
+    // Why we do that, the simple way that 
+    // "DOM manipulations has a cost" and if i can just manipulated one time is better than three 
+}
+
